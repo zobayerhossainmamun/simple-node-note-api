d37a7a67b0d32bb5efa155de563e13f6a8ed8246
Md Zobayer Hossain Mamun <zobayerhossainmamun@gmail.com>
2026-04-07 12:00:07 +0600

    fix issue

--- db.js
+++ db.js
@@ -1,22 +1,27 @@
 import pkg from "pg";
 import { getSecret } from "./secret.js";
 
-const { Client } = pkg;
+const { Pool } = pkg;
 
-let db;
+let pool;
 
 export const getDB = async () => {
-    if (db) return db;
+    if (pool) return pool;
 
     const secret = await getSecret();
 
-    db = new Client({
+    pool = new Pool({
         host: secret.host,
         user: secret.username,
         password: secret.password,
         database: secret.database,
+        port: 5432,
+
+        // IMPORTANT for Lambda stability
+        max: 2,
+        idleTimeoutMillis: 10000,
+        connectionTimeoutMillis: 5000,
     });
 
-    await db.connect();
-    return db;
+    return pool;
 };
